7713fbb35665947ed92bf94788c5f254dd2308ed
Hpn4 <senigout.etienne@gmail.com>
2026-01-13 23:48:52 +0100

    feat: readme

--- ui/talk.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-requests.post(
-    "http://localhost:5000/say",
-    json={"who": "joe", "text": "Welcome adventures in our tavern. Whant some beer or a story about a dark magician ?"}
-)
--- ui/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-requests.post(
-    "http://localhost:5000/scene",
-    json={"scene": "guard_barracks"}
-)
